751359edffb068682c6c6387e7b1f579f2f0a171
Mat Groves <mat@goodboydigital.com>
2016-01-05 10:43:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+module.exports = {
+
+    createContext: 			require('./lib/createContext'),
+    setVertexAttribArrays: 	require('./lib/setVertexAttribArrays'),
+    
+    GLBuffer: 				require('./lib/GLBuffer'),
+    GLFramebuffer: 			require('./lib/GLFramebuffer'),
+    GLShader: 				require('./lib/GLShader'),
+    GLTexture: 				require('./lib/GLTexture'),
+    
+    VertexArrayObject: 		require('./lib/VertexArrayObject')
+
+};
