90ef708ad6531b66164858b485b13fd8f94483c1
finscn <finscn@gmail.com>
2018-01-30 17:12:07 +0800

    Update extractUniforms.js

--- src/shader/extractUniforms.js
+++ src/shader/extractUniforms.js
@@ -11,27 +11,27 @@ var defaultValue = require('./defaultValue');
  */
 var extractUniforms = function(gl, program)
 {
-	var uniforms = {};
+    var uniforms = {};
 
     var totalUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
 
     for (var i = 0; i < totalUniforms; i++)
     {
-    	var uniformData = gl.getActiveUniform(program, i);
-    	var name = uniformData.name.replace(/\[.*?\]/, "");
+        var uniformData = gl.getActiveUniform(program, i);
+        var name = uniformData.name.replace(/\[.*?\]/, "");
         var type = mapType(gl, uniformData.type );
         var array = uniformData.name.indexOf('[') > 0;
 
-    	uniforms[name] = {
-    		type: type,
-    		size: uniformData.size,
-    		array: array,
-    		location: gl.getUniformLocation(program, name),
-    		value: defaultValue(type, uniformData.size)
-    	};
+        uniforms[name] = {
+            type: type,
+            size: uniformData.size,
+            array: array,
+            location: gl.getUniformLocation(program, name),
+            value: defaultValue(type, uniformData.size)
+        };
     }
 
-	return uniforms;
+    return uniforms;
 };
 
 module.exports = extractUniforms;
